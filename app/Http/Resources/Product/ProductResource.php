889673aa8085af23ddbc402087eade685bb70570
PADMA <padmavathy.droid01@gmail.com>
2019-08-13 15:01:55 +0530

    Modified Product Details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,13 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
+            'totalPrice' => round((1-($this->discount/100))* $this->price, 2),
+            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
             'discount' => $this->discount,
+            'rating' => $this->reviews->count() > 0 ?round($this->reviews->sum('star')/ $this->reviews()->count(),2): "No ratings yet",
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
